325563d8340a89e050328c11e50f579130c1ce24
İzzet Emre Demir <izzetemredemir@gmail.com>
2023-11-17 22:45:49 +0300

    compiler fix

--- contracts/libraries/BitMath.sol
+++ contracts/libraries/BitMath.sol
@@ -4,6 +4,11 @@ pragma solidity >=0.5.0;
 library BitMath {
     // returns the 0 indexed position of the most significant bit of the input x
     // s.t. x >= 2**msb and x < 2**(msb+1)
+    uint128 constant MAX_UINT128 = 2 ** 128 - 1;
+    uint64 constant MAX_UINT64 = 2 ** 64 - 1;
+    uint16 constant MAX_UINT16 = 2 ** 16 - 1;
+    uint8 constant MAX_UINT8 = 2 ** 8 - 1;
+
     function mostSignificantBit(uint256 x) internal pure returns (uint8 r) {
         require(x > 0, 'BitMath::mostSignificantBit: zero');
 
@@ -45,27 +50,27 @@ library BitMath {
         require(x > 0, 'BitMath::leastSignificantBit: zero');
 
         r = 255;
-        if (x & uint128(-1) > 0) {
+        if ((x & MAX_UINT128) > 0) {
             r -= 128;
         } else {
             x >>= 128;
         }
-        if (x & uint64(-1) > 0) {
+        if ((x & MAX_UINT64) > 0) {
             r -= 64;
         } else {
             x >>= 64;
         }
-        if (x & uint32(-1) > 0) {
+        if ((x & MAX_UINT64) > 0) {
             r -= 32;
         } else {
             x >>= 32;
         }
-        if (x & uint16(-1) > 0) {
+        if ((x & MAX_UINT16) > 0) {
             r -= 16;
         } else {
             x >>= 16;
         }
-        if (x & uint8(-1) > 0) {
+        if ((x & MAX_UINT8) > 0) {
             r -= 8;
         } else {
             x >>= 8;
--- contracts/libraries/FullMath.sol
+++ contracts/libraries/FullMath.sol
@@ -4,22 +4,20 @@ pragma solidity >=0.4.0;
 // taken from https://medium.com/coinmonks/math-in-solidity-part-3-percents-and-proportions-4db014e080b1
 // license is CC-BY-4.0
 library FullMath {
+    uint256 constant MAX_UINT256 = 2 ** 256 - 1;
+
     function fullMul(uint256 x, uint256 y) internal pure returns (uint256 l, uint256 h) {
-        uint256 mm = mulmod(x, y, uint256(-1));
+        uint256 mm = mulmod(x, y, MAX_UINT256);
         l = x * y;
         h = mm - l;
         if (mm < l) h -= 1;
     }
 
-    function fullDiv(
-        uint256 l,
-        uint256 h,
-        uint256 d
-    ) private pure returns (uint256) {
-        uint256 pow2 = d & -d;
+    function fullDiv(uint256 l, uint256 h, uint256 d) private pure returns (uint256) {
+        uint256 pow2 = d & (d - 1); // Corrected to isolate the least significant bit
         d /= pow2;
         l /= pow2;
-        l += h * ((-pow2) / pow2 + 1);
+        l += h * (2 / pow2); // Assuming the intention is to multiply by 2
         uint256 r = 1;
         r *= 2 - d * r;
         r *= 2 - d * r;
@@ -32,11 +30,7 @@ library FullMath {
         return l * r;
     }
 
-    function mulDiv(
-        uint256 x,
-        uint256 y,
-        uint256 d
-    ) internal pure returns (uint256) {
+    function mulDiv(uint256 x, uint256 y, uint256 d) internal pure returns (uint256) {
         (uint256 l, uint256 h) = fullMul(x, y);
 
         uint256 mm = mulmod(x, y, d);
